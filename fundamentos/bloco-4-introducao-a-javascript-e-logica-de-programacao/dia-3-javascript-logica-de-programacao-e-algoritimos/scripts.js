a363d554a36619ca289b7c51618ef2c09847c84d
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-03-15 18:29:01 -0300

    novas atividades

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritimos/scripts.js
@@ -0,0 +1,25 @@
+// Exercicio 1//
+
+let numbers = 10;
+let resul = 0;
+
+for (let index = 0; index> numbers.lenght; index++ ){
+    resul *= numbers[index];
+    console.log(resul);
+}
+
+
+
+// Exercicio 2 //
+
+let word = 'tryber';
+
+for (let index = 5; index >= 0 ; index -= 1){
+    console.log(word[index]);
+}
+
+// Exercicio 3 //
+
+let array =
+
+let maiorPalavra
