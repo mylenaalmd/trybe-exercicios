f5bfa354ae76f9f346bee8d4fba8fdbc1b7552e1
Mylena Nunes <mylenanunesdealmeida112@gmail.com>
2022-03-17 15:52:24 -0300

    novas atividades

---
+++ fundamentos/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/scripts.js
@@ -0,0 +1,31 @@
+// 1-
+const filhoBody = document.getElementsByTagName('body');
+const newTag = document.createElement("tag");
+newTag.tagName = "Exercício 5.2 - JavaScript DOM ";
+filhoBody.appendChild(newTag);
+
+// 2-
+
+const filhoBody2 = document.getElementsByTagName('body');
+const novaTag = document.getElementsByTagName("tag");
+novaTag.tagName = "main";
+filhoBody.appendChild(novaTag);
+
+const classeMain = document.getElementsByTagName("main");
+const novaClasse = document.getElementsByClassName("class");
+novaClasse.className = "main-content";
+classeMain.appendChild(novaClasse);
+
+// 3-
+
+// 4-
+// 5-
+// 6-
+// 7-
+// 8-
+// 9-
+// 1-
+// 2-
+// 3-
+// 4-
+// 5-
